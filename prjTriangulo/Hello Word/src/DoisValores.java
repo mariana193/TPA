96700ef5ebaf5ab4a45002a8648488ea6c22f8a3
mariana193 <marianamenesesalvesdasilva@gmail.com>
2025-10-07 11:23:56 -0300

    Add files via upload

---
+++ prjTriangulo/Hello Word/src/DoisValores.java
@@ -0,0 +1,14 @@
+
+public class DoisValores {
+	public static void main(String[] args) {
+		int a, b , c;
+		
+		a=5;
+		b=10;
+	
+	
+
+		
+	}
+
+}
---
+++ prjTriangulo/Hello Word/src/diasEmAnos.java
@@ -0,0 +1,22 @@
+
+public class diasEmAnos {
+	 public static void main(String [] args) {
+		 
+		 int dias, mes, ano,meses ,anos;
+		 
+		 dias=500;
+		 mes=12;
+		 ano=365;
+		 
+		 meses=(500/30);
+		 anos=(500/365);
+		 
+		 System.out.println("Dias em meses = " +meses+ " meses");
+		 System.out.println("Dias em anos = " +anos+ " ano");
+		 
+		 
+				 
+		 
+	 }
+
+}
