585283b94400bbb6e8fcc6b79eb5cfe927bee41a
mariana193 <marianamenesesalvesdasilva@gmail.com>
2025-10-07 11:40:12 -0300

    Add files via upload

---
+++ prjImpares/prjAula10/src/prjAula10/lacoImpar.java
@@ -0,0 +1,17 @@
+package prjAula10;
+
+public class lacoImpar {
+
+	public static void main(String[] args) {
+		int i;
+		double resultado;
+		resultado=1;
+		for(i=1;i<500;i++) {
+			resultado=resultado+2;
+			System.out.println("Os números ímpar de 1 a 1000 são " +resultado);
+			
+		}
+
+	}
+
+}
---
+++ prjImpares/prjAula10/src/prjAula10/lacoTabuada.java
@@ -0,0 +1,23 @@
+package prjAula10;
+
+public class lacoTabuada {
+
+	public static void main(String[] args) {
+		int i;
+		double multi,n;
+		multi=0;
+		for(i=1;i<=10;i++) {
+			System.out.println("Tabuada do" +i);
+			for(n=1;n<=10;n++) {
+				System.out.println(i+ "x" +n+ "="+(i*n));
+				
+				
+				
+				
+			}
+		
+			
+		}
+	}
+
+}
